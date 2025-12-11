888777c24d899d3d72d2b666b259feb1442f097a
Bart van Deenen <bartvandeenen@fastmail.fm>
2025-12-11 16:02:54 +0100

    feat(master): show-config tables

--- cmd/showConfig.go
+++ cmd/showConfig.go
@@ -3,11 +3,20 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"strings"
 	"svlogj/pkg/config"
+	"svlogj/pkg/types"
+	"svlogj/pkg/utils"
 
 	"github.com/spf13/cobra"
 	"gopkg.in/yaml.v3"
 )
+import (
+	"github.com/jedib0t/go-pretty/v6/table"
+)
+
+const tableFlag = "table"
 
 // createConfigCmd represents the createConfig command
 var showConfigCommand = &cobra.Command{
@@ -16,12 +25,58 @@ var showConfigCommand = &cobra.Command{
 	Long:  ` `,
 	Run: func(cmd *cobra.Command, args []string) {
 		conf := config.LoadConfig()
-		bytes, err := yaml.Marshal(conf)
-		cobra.CheckErr(err)
-		fmt.Println(string(bytes))
+		if utils.GetBool(cmd.Flags(), tableFlag) {
+			showConfigAsTable(conf)
+
+		} else {
+
+			bytes, err := yaml.Marshal(conf)
+			cobra.CheckErr(err)
+			fmt.Println(string(bytes))
+		}
 	},
+}
+
+func showConfigAsTable(c types.Config) {
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(table.Row{"Facilities", "Levels", "Entities", "Services"})
+	for i := range 1000 {
+		f := getOrEmpty(c.Facilities, i)
+		l := getOrEmpty(c.Levels, i)
+		e := getOrEmpty(c.Entities, i)
+		s := getOrEmpty(c.Services, i)
+		if len(f) == 0 && len(l) == 0 && len(e) == 0 && len(s) == 0 {
+			break
+		}
+		t.AppendRow([]interface{}{f, l, e, s})
+	}
+	t.Render()
+
+	fmt.Println("The services are the subdirectories in /var/log/socklog")
+	t = table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.AppendHeader(table.Row{"Service", "config"})
+
+	for _, s := range c.ConfigFiles {
+		lines := strings.Join(s.Lines, "\n")
+		t.AppendRow([]interface{}{s.Service, lines})
+		t.AppendSeparator()
+	}
+	t.Render()
+
+}
+
+func getOrEmpty(stringList []string, i int) string {
+	if i >= len(stringList) {
+		return ""
+	} else {
+		return stringList[i]
+	}
+
 }
 
 func init() {
 	rootCmd.AddCommand(showConfigCommand)
+	showConfigCommand.Flags().BoolP(tableFlag, "t", true, "table format")
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.25.4
 
 require (
 	github.com/adrg/xdg v0.5.3
+	github.com/jedib0t/go-pretty/v6 v6.7.7
 	github.com/spf13/cobra v1.10.2
 	github.com/spf13/pflag v1.0.9
 	gopkg.in/yaml.v3 v3.0.1
@@ -11,5 +12,9 @@ require (
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	golang.org/x/sys v0.26.0 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/term v0.29.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
 )
